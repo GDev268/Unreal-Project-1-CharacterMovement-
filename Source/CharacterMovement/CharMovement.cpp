bfe258c6219a45b3875ca54ffff0b45a2bb17749
GDev <gcgranja@gmail.com>
2022-08-03 19:35:24 +0100

    Last Commit

--- Source/CharacterMovement/CharMovement.cpp
+++ Source/CharacterMovement/CharMovement.cpp
@@ -17,7 +17,7 @@ ACharMovement::ACharMovement()
 	//Pointers for character Movement
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
-	GetCharacterMovement()->JumpZVelocity = 600.0f;
+	GetCharacterMovement()->JumpZVelocity = 450.0f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
 	//Creation of the camera and attach to the rootComponent AKA: Capsule Component
@@ -33,6 +33,28 @@ ACharMovement::ACharMovement()
 	CameraFollow->SetupAttachment(ThaCamera, USpringArmComponent::SocketName);
 
 	CameraFollow->bUsePawnControlRotation = false;
+}
+
+void ACharMovement::MoveFoward(float Axis)
+{
+	//Rotate the character
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	//Move the character in the X Axis
+	FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(Direction, Axis);
+}
+
+void ACharMovement::MoveRight(float Axis)
+{
+	//Rotate the character
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	//Move the character in the Y Axis
+	FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(Direction, Axis);
 }
 
 // Called when the game starts or when spawned
@@ -53,6 +75,18 @@ void ACharMovement::Tick(float DeltaTime)
 void ACharMovement::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Move the camera
+	PlayerInputComponent->BindAxis("TurnX", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnY", this, &APawn::AddControllerPitchInput);
+
+	//Jump the character
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	//Move the character
+	PlayerInputComponent->BindAxis("Horizontal", this, &ACharMovement::MoveRight);
+	PlayerInputComponent->BindAxis("Vertical", this, &ACharMovement::MoveFoward);
 
 }
 
